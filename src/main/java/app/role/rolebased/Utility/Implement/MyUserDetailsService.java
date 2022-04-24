c41fa8aa12ebc0bcd809cb7b114eb27ecee8f2ba
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 15:15:43 +0300

    RoleBased

---
+++ src/main/java/app/role/rolebased/Utility/Implement/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package app.role.rolebased.Utility.Implement;
+
+import app.role.rolebased.Domain.User;
+import app.role.rolebased.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userRes = userRepo.findByEmail(email);
+        if(userRes.isEmpty())
+            throw new UsernameNotFoundException("Could not findUser with email = " + email);
+        User user = userRes.get();
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
